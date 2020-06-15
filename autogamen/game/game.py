f86bcc3a08b006256ec9f1f09c90b7aa93011293
Dorian Johnson <2019@dorianj.net>
2020-06-15 14:17:30 -0700

    run_headless_game now runs multiple matches

--- autogamen/game/game.py
+++ autogamen/game/game.py
@@ -1,3 +1,4 @@
+import logging
 import random
 import time
 
@@ -67,7 +68,7 @@ class Game:
     self.active_dice = self.roll_starting()
     self.active_color = Color.White if self.active_dice.roll[0] > self.active_dice.roll[1] else Color.Black
 
-    print(f"First roll: {self.active_dice.roll}; starting player: {self.active_color}")
+    logging.debug(f"First roll: {self.active_dice.roll}; starting player: {self.active_color}")
 
   def end(self, winner, points):
     self.winner = winner
@@ -90,7 +91,7 @@ class Game:
     self.active_color = self.active_color.opponent()
     self.active_dice = Dice()
     self.turn_number += 1
-    print(f"Turn {self.turn_number}: {self.active_color} has roll {self.active_dice.roll}:")
+    logging.debug(f"Turn {self.turn_number}: {self.active_color} has roll {self.active_dice.roll}:")
 
     possible_moves = self.board.possible_moves(self.active_color, self.active_dice)
     turn_result = self.active_player().action(set(possible_moves))
@@ -98,19 +99,19 @@ class Game:
 
     if turn_action is TurnAction.Move:
       moves = turn_result[1]
-      print(f"\tMoves: {moves}")
+      logging.debug(f"\tMoves: {moves}")
 
       if moves not in possible_moves:
         raise Exception("Illegal move attempted!")
 
       [self.board.apply_move(move) for move in moves]
     elif turn_action is TurnAction.DoublingCube:
-      print(f"\tOffers the doubling cube")
+      logging.debug(f"\tOffers the doubling cube")
       raise Exception("Doubling cube is unimplemented")
     elif turn_action is TurnAction.Pass:
       if len(possible_moves):
         raise Exception("Illegal pass attempted!")
-      print("\tNo play available, player passes")
+      logging.debug("\tNo play available, player passes")
     else:
       raise Exception(f"Unknown turn action {turn_action}")
 
--- run_headless_game.py
+++ run_headless_game.py
@@ -1,5 +1,6 @@
 from collections import Counter
 import argparse
+import logging
 import random
 import sys
 
@@ -10,10 +11,15 @@ from autogamen.game.types import Color
 parser = argparse.ArgumentParser()
 parser.add_argument("white", default="Bozo")
 parser.add_argument("black", default="Bozo")
-parser.add_argument("--match_points", help="Points for the match", default=10)
-parser.add_argument("--matches", help="Number of matches to play", default=1)
-print(parser.parse_args())
+parser.add_argument("--match_points", help="Points for the match", default=10, type=int)
+parser.add_argument("--matches", help="Number of matches to play", default=1, type=int)
+parser.add_argument("--parallelism", help="Number processes to use", default=1, type=int)
+parser.add_argument("--seed", help="Random seed, for determistic testing", type=int)
+parser.add_argument("--verbosity", help="Logging verbosity (debug/info/warning)", default="info")
 
+
+def _fmt_percent(p):
+  return "{0:.1%}".format(p)
 
 def _run_match(white_player_cls, black_player_cls, point_limit):
   match = Match([white_player_cls(Color.White), black_player_cls(Color.Black)], point_limit)
@@ -22,19 +28,39 @@ def _run_match(white_player_cls, black_player_cls, point_limit):
     if match.tick():
       if match.winner is not None:
         game_count = len(match.games)
-        print(f"Match ended! {match.winner.color} won with {match.points[match.winner.color]} points in {game_count} games with {match.turn_count} turns")
-        for color, win_count in Counter(game.winner.color for game in match.games).items():
-          print(f"{color} won {win_count} games ({win_count / game_count * 100}%)")
+        logging.info(f"Match ended: {match.winner.color} won with {match.points[match.winner.color]} points in {game_count} games with {match.turn_count} turns")
+        for color, wins in Counter(game.winner.color for game in match.games).items():
+          logging.info(f"{color} won {wins} games ({_fmt_percent(wins / game_count)})")
         return match
       else:
         game = match.current_game
-        print(f"Game ended! {game.winner.color} won with {game.points} points after {game.turn_number} turns.")
+        logging.info(f"Game ended: {game.winner.color} won with {game.points} points after {game.turn_number} turns.")
         match.start_game()
-
 
 if __name__ == "__main__":
   args = parser.parse_args()
-  print(args.white)
-  players = [globals()[f"{name}Player"] for name in (args.white, args.black)]
-  match = _run_match(*players, args.match_points)
+
+  # Housekeeping: log levels
+  numeric_level = getattr(logging, args.verbosity.upper(), None)
+  if not isinstance(numeric_level, int):
+    raise ValueError('Invalid log level: %s' % loglevel)
+  logging.basicConfig(level=numeric_level, format="%(asctime)s: %(message)s")
+
+  # Random seed
+  if args.seed:
+    random.seed(args.seed)
+
+  # Initialize players
+  players_cls = [globals()[f"{name}Player"] for name in (args.white, args.black)]
+
+  # Run the matches.
+  matches = [_run_match(*players_cls, args.match_points) for i in range(0, args.matches)]
+
+  # Print results
+  print(f"{sum(len(match.games) for match in matches)} games finished:")
+  win_counts = Counter(match.winner.color for match in matches)
+  for player in matches[0].players:
+    wins = win_counts[player.color]
+    print(f"{type(player).__name__} ({player.color}) won {wins} matches ({_fmt_percent(wins / len(matches))})")
+
   sys.exit(0)
