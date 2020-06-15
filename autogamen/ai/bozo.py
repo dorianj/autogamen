d014d299fc86690241fb4a4c96f2ec1328254dd8
Dorian Johnson <2019@dorianj.net>
2020-06-15 11:36:43 -0700

    bozo: document player behavior

--- autogamen/ai/bozo.py
+++ autogamen/ai/bozo.py
@@ -5,6 +5,9 @@ from autogamen.game.player import Player
 from autogamen.game.types import TurnAction
 
 class BozoPlayer(Player):
+  """This player picks a random move every time, and never accepts
+     the doubling cube.
+  """
   def action(self, possible_moves):
     if not len(possible_moves):
       return [TurnAction.Pass]
