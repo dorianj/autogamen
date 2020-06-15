c3db0b0e164690ccdb2ccf73c87e5c54cc5754dd
Dorian Johnson <2019@dorianj.net>
2020-06-15 10:59:54 -0700

    Match: implement bounded matches

--- autogamen/game/match.py
+++ autogamen/game/match.py
@@ -1,14 +1,22 @@
+from collections import Counter
+
 from .game import Game
+from .types import Color
 
 class Match:
-  def __init__(self, players):
+  def __init__(self, players, point_goal):
     self.players = players
+    self.point_goal = point_goal
+
+    self.games = []
+    self.points = {Color.White: 0, Color.Black: 0}
 
   def start(self):
     self.start_new_game()
 
   def start_new_game(self):
     self.current_game = Game(self.players)
+    self.games.append(self.current_game)
     self.current_game.start()
 
   def tick(self):
@@ -16,4 +24,15 @@ class Match:
 
     if self.current_game.winner:
       print(f"Game ended! {self.current_game.winner.color} won with {self.current_game.points} points")
+      self.points[self.current_game.winner.color] += self.current_game.points
+
+      if self.points[self.current_game.winner.color] >= self.point_goal:
+        game_count = len(self.games)
+        print(f"Match ended! {self.current_game.winner.color} won with {self.points[self.current_game.winner.color]} points in {game_count} games")
+        for color, win_count in Counter(game.winner.color for game in self.games).items():
+          print(f"{color} won {win_count} games ({win_count / game_count * 100}%)")
+        return True
+
       self.start_new_game()
+
+    return False
--- run_headless_game.py
+++ run_headless_game.py
@@ -2,11 +2,14 @@ from autogamen.ai.bozo import BozoPlayer
 from autogamen.game.match import Match
 from autogamen.game.types import Color
 
+import sys
+
 import random
 random.seed(1234)
 
-match = Match([BozoPlayer(Color.White), BozoPlayer(Color.Black)])
+match = Match([BozoPlayer(Color.White), BozoPlayer(Color.Black)], 25)
 
 match.start()
 while True:
-  match.tick()
+  if match.tick():
+    sys.exit()
