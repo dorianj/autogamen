e4d0f7d8fa617351543cc1768ecd1a0cdb3a57b4
Dorian Johnson <2019@dorianj.net>
2020-06-18 17:13:30 -0700

    abandon neuro-evolution in favor of reinforcement learning

--- autogamen/ai/mlp.py
+++ autogamen/ai/mlp.py
@@ -19,18 +19,25 @@ class Net(nn.Module):
     Outputs: 4, probability white or black wins or with a gammon
   """
 
-  hidden_neurons = 28
+  input_neurons = 190
+  hidden_neurons = 20
 
-  def __init__(self, weights):
+  def __init__(self):
     super().__init__()
     self.layers = nn.Sequential(
+      nn.Linear(self.input_neurons, self.hidden_neurons),
+      nn.Sigmoid(),
       nn.Linear(28, self.hidden_neurons),
       nn.Sigmoid(),
       nn.Linear(self.hidden_neurons, 4)
     )
-    self.weights = weights # TODO: apply weights
+
+    with torch.no_grad():
+      print(f"layers@0: {self.layers[0]}")
+
 
   def vectorize_board(self, board):
+    # TODO: change this
     board_state = [point.count * (1 if point.color == Color.White else -1)
                    for point in board.points]
     for color in Color:
@@ -42,63 +49,60 @@ class Net(nn.Module):
   def forward(self, x):
     return self.layers(x)
 
-  def breed(self, other):
-    """Combine weights with :other: Net, returns a new Net
-    """
-    return Net(list(self.weights))
-
-  def mutate(self, factor):
-    """Randomly change weights; returns a new Net
-    """
-    new_weights = list(self.weights)
-    return Net(new_weights)
-
-
-  @classmethod
-  def random_net(cls):
-    return Net([
-      torch.randn(28),
-      torch.randn(cls.hidden_neurons),
-      torch.randn(4),
-    ])
+  def update_weights(self, p, p_next):
+    pass
 
 
 class MLPPlayer(Player):
-  """Picks a random move every time.
+  """Picks the best move in the universe every time.
   """
-  def __init__(self, color, net):
+  def __init__(self, color, net, learning=False):
     super().__init__(color)
     self.net = net
+    self.learning = learning
 
-  def score_board(self, board):
-    weights = self.net(self.net.vectorize_board(board))
-    [white_wins, black_wins, white_gammons, black_gammons] = weights
+  def p(self, board):
+    return self.net(self.net.vectorize_board(board))
 
+  def score(self, p):
+    [white_wins, black_wins, white_gammons, black_gammons] = p
     if self.color == Color.White:
       return white_wins * 1 + white_gammons * 2
     else:
       return black_wins * 1 + black_gammons * 2
 
-
   def action(self, possible_moves):
     if not len(possible_moves):
       return [TurnAction.Pass]
+
+    if self.learning:
+      p = self.p(self.game.board)
 
     possible_boards = set((self.game.board.clone_apply_moves(moves).frozen_copy(), moves)
                           for moves in possible_moves)
 
     best_score = -10000
+    best_p = None
     best_moves = None
     for (board, moves) in possible_boards:
-      score = self.score_board(board)
+      p = self.p(board)
+      score = self.score(p)
       if score > best_score:
         best_score = score
         best_moves = moves
+        best_p = p
+
+    if self.learning:
+      self.net.update_weights(p, p_next)
 
     return [TurnAction.Move, best_moves]
 
   def accept_doubling_cube(self):
     return False
 
-  ##
+  def end_game(self, game):
+    super().end_game(game)
+
+    if self.learning:
+      """TODO"""
 
---
+++ run_reinforcement_learning.py
@@ -0,0 +1,69 @@
+from collections import Counter
+from datetime import datetime
+import argparse
+import itertools
+import logging
+import math
+import os.path
+import pickle
+import random
+import sys
+
+from autogamen.ai.mlp import MLPPlayer, Net
+from autogamen.game.game import Game
+from autogamen.game.match import Match
+from autogamen.game.types import Color
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--games", help="Number of games to play", default=10, type=int)
+parser.add_argument("--checkpoint", help="Save model after this many training games", default=1000, type=int)
+parser.add_argument("--alpha", help="Learning rate", default=0.1, type=float)
+parser.add_argument("--verbosity", help="Logging verbosity (debug/info/warning)", default="info")
+
+args = parser.parse_args()
+
+def _fmt_percent(p):
+  return "{0:.1%}".format(p)
+
+
+def net_directory():
+  return os.path.join(os.path.dirname(os.path.realpath(__file__)), 'nets')
+
+
+def write_net_to_file(nets, path):
+  with open(path, 'wb') as fp:
+    pickle.dump([net.weights for net in nets], fp)
+
+
+def run_game(white, black, net):
+  game = Game([white, black])
+  game.start()
+
+  for turn_number in itertools.count():
+    game.run_turn()
+    if game.winner:
+      return
+
+
+if __name__ == "__main__":
+  run_timestamp = datetime.now().isoformat()
+  print(f"Running learning for {args.games} games Checkpointing every "
+        f"{args.checkpoint} games to timestamp {run_timestamp}")
+
+  # Housekeeping: log levels
+  numeric_level = getattr(logging, args.verbosity.upper(), None)
+  if not isinstance(numeric_level, int):
+    raise ValueError('Invalid log level: %s' % loglevel)
+  logging.basicConfig(level=numeric_level, format="%(asctime)s: %(message)s")
+
+  net = Net()
+  [white, black] = [MLPPlayer(Color.White, net, True), MLPPlayer(Color.Black, net, True)]
+  for i in range(0, args.games + 1):
+    if i % args.checkpoint:
+      print(f"Checkpointing at {i}")
+      write_net_to_file(nets, os.path.join(net_directory(), f"net-{run_timestamp}-{gen}.pickle"))
+
+    run_game(white, black, net)
+
+
+  sys.exit(0)
